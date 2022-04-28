36942cc0de04bc2e040ece3a59d7bdaedda77a25
Caleb Pardeik <7912201@isd535.org>
2022-04-28 10:39:02 -0500

    move files

---
+++ dischord.js
@@ -0,0 +1,5 @@
+//token
+// OTY5MjU5MDExODc2NzI0NzM2.YmqyzA.Kdxpokk3VqIgbWSqY1lO8T7ksgg
+
+//oauth link
+// https://discord.com/api/oauth2/authorize?client_id=969259011876724736&permissions=8&scope=bot
